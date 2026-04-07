f5f668f4012bcd394f63c849588137be81ba4165
Alkeshk26 <thelearner2026@gmail.com>
2026-04-07 15:51:53 +0530

    adding some question

---
+++ 12.py
@@ -0,0 +1,16 @@
+# Number Guessing Game
+
+import random
+
+number = random.randint(1, 100)
+
+while True:
+    guess = int(input("Guess a number (1-100): "))
+
+    if guess == number:
+        print("Correct! You guessed it 🎉")
+        break
+    elif guess < number:
+        print("Too low")
+    else:
+        print("Too high")
